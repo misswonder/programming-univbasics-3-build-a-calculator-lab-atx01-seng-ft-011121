5d0bb1aeeb0540abcddef54264c93149c53b11ab
Yvonne Chen <yvonnechen1991@gmail.com>
2020-12-12 04:59:41 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,22 +1,37 @@
 def addition(num1, num2)
-
+  sum = num1 + num2
+  puts sum
+  sum
 end
+addition
 
 def subtraction(num1, num2)
-
+  num = num1 - num2 
+  puts num
+  num
 end
+subtraction
 
 def division(num1, num2)
-
+  num = num1 / num2
+  puts num
+  num
 end
+division
 
 def multiplication(num1, num2)
-
+  num = num1 * num2
+  puts num
+  num
 end
+multiplication
 
 def modulo(num1, num2)
-
+  num = num1 % num2
+  puts num
+  num
 end
+modulo
 
 def square_root(num)
 
